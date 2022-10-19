1aa9bc70e33671b4db8210f6f5ba43a29108631c
Andrew <ammcleod2000@gmail.com>
2022-10-19 19:07:01 -0400

    adding login and logout functionality

---
+++ app/pages/index.js
@@ -0,0 +1,10 @@
+import Nav from "../components/Nav"
+import LoginButton from "../components/LoginButton"
+import { SessionProvider } from "next-auth/react"
+import MyApp from "./_app"
+
+export default function Home() {
+  return (
+      <MyApp></MyApp>
+  )
+}
