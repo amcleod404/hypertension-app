3fc0d39cc8336f04d499778a585038ed098eaf97
Ian Boldea <ianfboldea@gmail.com>
2022-11-06 10:54:15 -0500

    Connected MongoDB

--- app/pages/api/auth/[...nextauth].js
+++ app/pages/api/auth/[...nextauth].js
@@ -4,7 +4,16 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      console.log("HAHAHAHHAH")
+      const { MongoClient, ServerApiVersion } = require('mongodb');
+      const uri = "mongodb+srv://<username>:<password>@hypertension-cluster.vkdattq.mongodb.net/?retryWrites=true&w=majority";
+      const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+      client.connect(err => {
+        const collection = client.db("auth").collection("users");
+        console.log(collection)
+        // perform actions on the collection object
+        client.close();
+      });
+
       console.log(user)
       // Plug in mongodb here
       return true
